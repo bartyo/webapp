02afec0ce7f8849f89db85b3bba1648c04c06e67
Kmelow <camilo.c@protonmail.com>
2019-09-16 13:29:53 +0200

    Create authentication middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const auth = require('../../middleware/auth');
 const router = express.Router();
 
+const User = require('../../models/User');
+
 // @route 	GET api/auth
-// @desc		Test route
+// @desc		Validate token to authenticate access
 // @access	Public
 
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+	try {
+		const user = await User.findById(req.user.id).select('-password');
+		res.status(200).json(user);
+	} catch (err) {
+		console.error(err.message);
+		res.status(401).json({ errors: [{ msg: 'Invalid user' }] });
+	}
+});
 
 module.exports = router;
