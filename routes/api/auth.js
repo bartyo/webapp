6b18ba83eaa4668e8703068e1267fc5ae6443aba
Kmelow <camilo.c@protonmail.com>
2019-09-16 14:21:56 +0200

    Authenticate signin

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const auth = require('../../middleware/auth');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
 
 const User = require('../../models/User');
@@ -17,5 +21,55 @@ router.get('/', auth, async (req, res) => {
 		res.status(401).json({ errors: [{ msg: 'Invalid user' }] });
 	}
 });
+
+// @route 	POST api/auth
+// @desc		Authenticate user and get token
+// @access	Public
+
+router.post(
+	'/',
+	auth,
+	[
+		check('email', 'Please include valid e-mail').isEmail(),
+		check('password', 'Password required').exists()
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		// Extract data sent through body of request
+		const { email, password } = req.body;
+
+		try {
+			// Check if email exists
+			let user = await User.findOne({ email });
+			let pwdMatch = null;
+
+			if (user) {
+				pwdMatch = await bcrypt.compare(password, user.password);
+			}
+
+			if (!user || !pwdMatch) {
+				return res
+					.status(400)
+					.json({ errors: [{ msg: 'Invalid credentials' }] });
+			}
+
+			jwt.sign(
+				{ user: { id: user.id } },
+				keys.jwtSecret,
+				{ expiresIn: '365 days' },
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server error');
+		}
+	}
+);
 
 module.exports = router;
