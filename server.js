57d895ac9f2b5173a4f58d1df3ea8aee1c89028d
Kmelow <camilo.c@protonmail.com>
2019-09-20 18:32:22 +0200

    Deploying initial version of front-end

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const path = require('path');
 
 // DB connection
 mongoose
@@ -20,11 +21,19 @@ const app = express();
 app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => res.send('API running good...'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/patients', require('./routes/api/patients'));
 app.use('/api/relays', require('./routes/api/relays'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
