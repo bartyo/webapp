9d12dd0f7e7f89d6fd6a6dd9ebfb4b29c8ea3e02
Kmelow <camilo.c@protonmail.com>
2019-09-24 22:05:14 +0200

    Test

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -15,10 +15,12 @@
 // 	}
 // }
 
-import { REMOVE_ALERT } from '../actions/types';
+import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
 
 export default function(state = [], action) {
 	switch (action.type) {
+		case SET_ALERT:
+			return [ ...state, action.payload ];
 		case REMOVE_ALERT:
 			return state.filter((alert) => alert.id !== action.payload);
 		default:
